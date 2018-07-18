238e1e54a3b6b03014d56991dde5a90ca72b0bd0
mingyu <rlaalsrbgk@gmail.com>
2018-07-18 12:26:30 +0900

    SnippetModelSerializer구현

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'pygments',
     'snippets',
 
     'django.contrib.admin',
---
+++ app/snippets/serializers.py
@@ -0,0 +1,34 @@
+from django.forms import widgets
+from rest_framework import serializers
+from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    class Meta:
+        model = Snippet
+        fields = (
+            'pk',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+        )
+
+    def create(self, validated_data):
+        """
+        검증한 데이터로 새 `Snippet` 인스턴스를 생성하여 리턴합니다.
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        검증한 데이터로 기존 `Snippet` 인스턴스를 업데이트한 후 리턴합니다.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
